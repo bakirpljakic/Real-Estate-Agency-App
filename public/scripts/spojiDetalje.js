e4a5358fd97f23a4a19bc463709c60977f2f3a68
Bakir Pljakic <bpljakic1@etf.unsa.ba>
2024-01-19 23:24:20 +0100

    Male izmjene

--- public/scripts/spojiDetalje.js
+++ public/scripts/spojiDetalje.js
@@ -74,18 +74,3 @@ function prikaziFormuZaUpit() {
     noviUpitContainer.style.display = 'block';
 }
 
-// Funkcija za postavljanje novog upita
-function postaviUpit() {
-    const noviUpitTekstInput = document.getElementById('noviUpitTekst');
-    const noviUpitTekst = noviUpitTekstInput.value;
-
-    PoziviAjax.postUpit(nekretninaId, noviUpitTekst, function (error, response) {
-        if (!error) {
-            noviUpitTekstInput.value = '';
-            location.reload();
-           // console.log('Upit uspješno postavljen!');
-        } else {
-            console.error('Greška pri postavljanju upita:', error);
-        }
-    });
-}
